bdab3376acba7b84ba08b907bfad645f480f00ec
Anatoly Belikov <abelikov@singularitynet.io>
2023-08-01 16:41:50 +0300

    example of hypernet with lpw

---
+++ examples/prompt2im_hypernet.py
@@ -0,0 +1,19 @@
+from multigen.prompting import Cfgen
+from multigen.sessions import GenSession
+from multigen.pipes import Prompt2ImPipe
+
+
+prompt = "photo of a (digitalben:1.1) farmer, man is on a farm next to his horse, 24mm, 4k textures, soft cinematic light, RAW photo, photorealism, photorealistic, highly detailed, sharp focus, soothing tones, insane details, intricate details, hyperdetailed, low contrast, soft cinematic light, dim colors, exposure blend, hdr, faded" 
+
+negative_prompt = "(deformed, distorted, disfigured:1.3), poorly drawn, bad anatomy, wrong anatomy, extra limb, missing limb, floating limbs, (mutated hands and fingers:1.4), disconnected limbs, mutation, mutated, ugly, disgusting, blurry, amputation" 
+
+
+path = "/home/imgen/models/icb_diffusers_final/"
+path_hypernet = '/home/imgen/models/hypernetworks/digitalben.pt'
+pipe = Prompt2ImPipe(path, lpw=True, scheduler='DPMSolverMultistepScheduler')
+pipe.setup(width=512, height=512, guidance_scale=5.5, clip_skip=1)
+pipe.add_hypernet(path_hypernet, 0.65)
+# pipe.clear_hypernets()
+
+gs = GenSession("./benben", pipe, Cfgen(prompt, negative_prompt, seeds=[1877029948]))
+gs.gen_sess(add_count=1)
